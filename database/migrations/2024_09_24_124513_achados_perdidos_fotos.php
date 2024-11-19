4689ed925f1446026e78cde725372bcaf7c66f28
Ana Lívia <vasconcellosanalivia89@gmail.com>
2024-11-19 08:36:22 -0300

    Voltando a migration

--- database/migrations/2024_09_24_124513_achados_perdidos_fotos.php
+++ database/migrations/2024_09_24_124513_achados_perdidos_fotos.php
@@ -3,28 +3,27 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use SebastianBergmann\Type\VoidType;
 
 return new class extends Migration
 {
     /**
      * Run the migrations.
      */
+
+
     public function up(): void
     {
-        Schema::create('usuarios', function (Blueprint $table) {
+        Schema::create('fotos_achadoseperdidos', function (Blueprint $table) {
             $table->id();
-            $table->string('name'); 
-            $table->string('email')->unique();
-            $table->string('password'); 
+            $table->binary('source');
+            $table->string('descricao');
+            $table->string('airline');
             $table->timestamps();
         });
     }
-
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::dropIfExists('usuarios');
+        //
     }
 };
